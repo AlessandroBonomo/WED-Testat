3c1a713e84864644e722a5609355152cf5103c07
Alessandro <abonomo@hsr.ch>
2017-10-24 16:26:48 +0200

    temp

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -1,8 +1,76 @@
 const notices = require("../services/orderStore.js");
 
 
+module.exports.node = function (req, res) {
+    res.type('text/html');
+    res.write("<html>");
+
+    res.write("<body>");
+    res.write("<label>" + "Title" + "</label>" +
+        "<input name='nodeTitle' type='text'/>" +
+        "<label>" + "Beschreibung" + "</label>" +
+        "<textarea name='nodeDescription'></textarea>" +
+        "<label>" + "Wichtigkeit" + "</label>" +
+        "<label>" + "Erledigt bis:" + "</label>" +
+        "<input name='date' type='date'/>" +
+        "<input name='btnSave' type='submit' value='Speichern'/>" +
+        "<input name='btnCancel' type='submit' value='Cancel'/>");
+    res.write("</body>");
+    res.end("</html>");
+};
+
+module.exports.mainMenu = function (req, res) {
+    res.type('text/html');
+    res.write("<html>");
+
+    res.write("<style>" +
+        ".article { height: 100px;}" +
+        ".title { margin-right: auto; margin-left: auto;}" +
+        ".descriptionArea { width: 60%; height: 100%; margin-left: auto; margin-right: auto;}" +
+        ".editButton {margin-right: 10px; width: 10%;}" +
+        ".inputDiv {height: 60px; display: flex;}" +
+        ".titleDiv {display: flex;}" +
+        ".finishedCheckbox { float: left;}" +
+        "</style>");
+
+    res.write("<body>");
+    //res.write("<input name='btnNewNode' type='submit' value='Create new Note'/>");
+    //res.write("<input name='btnSwitch' type='submit' value='Switch Style'>");
+    //res.write("<input name='btnFinishDate' type='submit' value='By finish Date'/>");
+    //res.write("<input name='btnCreateDate' type='submit' value='By created Date'/>");
+    //res.write("<input name='btnImportance' type='submit' value='By Importance'/>");
+    //res.write("<input name='btnShowFinished' type='submit' value='Show finished'/>");
+
+    res.write("<form action='/node' method='get'>" +
+        "<input type='submit' value='Create new Note'>" +
+        "<input type='submit' value='Switch Style'>" +
+        "<input type='submit' value='By finish Date'/>" +
+        "<input type='submit' value='By created Date'/>" +
+        "<input type='submit' value='By Importance'/>" +
+        "<input type='submit' value='Show finished'/>" +
+        "</form>");
 
 
+    for(i = 0; i < 5; i++) {
+        res.write("<article class='article'>" +
+            "<div class='titleDiv'>" +
+            "<label name='date'>" + "Date" + "</label>" +
+            "<label class='title' name='title'>" + "Titel" + "</label>" +
+            "</div>" +
+            "<div class='inputDiv'>" +
+            "<label>" +
+            "<input class='finishedCheckbox' type='checkbox' value='Finished'/>" +
+            "Finished" +
+            "</label>" +
+            "<textarea class='descriptionArea'></textarea>" +
+            "<input type='submit' value='Edit' class='editButton'/>" +
+            "</div>" +
+            "</article>");
+    }
+
+    res.write("</body>");
+    res.end("</html>");
+};
 
 
 
@@ -20,7 +88,8 @@ module.exports.showIndex = function(req, res)
         res.write("<html>");
         res.write("< Willkommen! Zu der besten Pizzaria auf der Welt!</p>");
         res.write("<img src='/images/pizza.jpg'>");
-        res.write("<form action='/notice' method='get'><input type='submit' value='Add a Notice'></form>");
+        //res.write("<form action='/notice' method='get'><input type='submit' value='Add a Notice'></form>");
+        res.write("<form action='/main' method='get'><input type='submit' value='Add a Notice'></form>");
         res.write("" + notices.all);
         res.end("</html>");
 
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const notice = require('../controller/ordersController.js');
 
 router.get("/", notice.showIndex); // Startseite
+router.get("/main", notice.mainMenu);
+router.get("/node", notice.node);
 router.get("/notice", notice.createNotice); //create Notice Post notice
 router.post("/notice", notice.addNotice); // addNotice
 //router.get("/notice", notice.); // Übersicht zu den Notizen
